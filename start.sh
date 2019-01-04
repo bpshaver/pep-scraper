d3fb9fc9943de04c098cd36dc3c3224f42d7f01f
benshaver <ben.shaver@cynnovative.com>
2019-01-04 16:20:53 -0500

    Now the scraper cant connect to the db

--- start.sh
+++ start.sh
@@ -1,7 +1,10 @@
 #! /bin/sh
 # docker-compose up
-docker-compose up --abort-on-container-exit
+docker-compose up -V --abort-on-container-exit
 # docker-compose up -d # deteach
 
 # You can have -d or --abort-on-container-exit but not both
 # I want abort-on-container-exit while debugging
+
+# -V :  -V, --renew-anon-volumes
+# Recreate anonymous volumes instead of retrieving data from the previous containers.
